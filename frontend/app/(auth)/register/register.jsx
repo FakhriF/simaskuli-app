08fa205c58a2209eaf93d0990b0ae975a73ec4b0
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2024-03-28 18:34:36 +0700

    Update Teacher Registration

--- frontend/app/(auth)/register/register.jsx
+++ frontend/app/(auth)/register/register.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
+import { addToken } from "@/app/(general)/actions";
+import { LoadingModal } from "@/app/(general)/components/loading";
 
 export default function RegisterForm() {
     const [tab, setTab] = useState("student");
@@ -54,31 +57,66 @@ function StudentRegisterForm() {
     const [password, setPassword] = useState("");
     const [confirmPass, setConfirmPass] = useState("");
 
+    // Error Notification
+    const [error, setError] = useState("");
+    const [nameError, setNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPassError, setConfirmPassError] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setLoading(true);
+
+        // set all error to empty
+        setError("");
+        setNameError("");
+        setEmailError("");
+        setPasswordError("");
+        setConfirmPassError("");
+
+        if (password !== confirmPass) {
+            setConfirmPassError("Password tidak sama");
+            return;
+        }
+
         try {
-            const response = await axios.post(
-                "http://localhost:8000/api/users",
-                {
+            const response = await axios
+                .post("http://localhost:8000/api/register", {
                     name: name,
                     email: email,
                     password: password,
-                    birthdate: birthDate,
+                    birthDate: birthDate,
                     role: "student",
-                }
-            );
-            if (response.status === 201) {
-                // set the token in local storage
-                localStorage.setItem("id_user", response.data.user.id);
-
-                router.push("/profile");
-            } else {
-                // setError(response.data.message);
-            }
+                })
+                .then((res) => {
+                    if (res.status === 201) {
+                        addToken(res.data.access_token);
+                        // console.log(res.data.access_token);
+                        router.push("/profile");
+                    } else {
+                        setError(res.data.message);
+                    }
+                });
         } catch (error) {
-            // setError(error.response.data.message);
+            console.log(error);
+            if (error.response.data.errors.name) {
+                setNameError(error.response.data.errors.name);
+            } else if (error.response.data.errors.email) {
+                setEmailError(error.response.data.errors.email);
+            } else if (error.response.data.errors.password[0]) {
+                setPasswordError(error.response.data.errors.password[0]);
+            } else {
+                setError(error.response.data.message);
+            }
+
+            setLoading(false);
+
         }
     };
 
@@ -88,16 +126,23 @@ function StudentRegisterForm() {
             autoComplete="off"
             onSubmit={handleSubmit}
         >
+            {loading && <LoadingModal showModal={loading} />}
             <h1 className="text-2xl font-semibold mb-4">Student Register</h1>
+            {error && <p className="text-red-500">{error}</p>}
             <div className="mb-4">
                 <label className="block mb-2">Nama</label>
                 <input
                     type="text"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        nameError && "border-red-500"
+                    }`}
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     required
                 />
+                {nameError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-4">
                 <label className="block mb-2">Tanggal Lahir</label>
@@ -113,31 +158,46 @@ function StudentRegisterForm() {
                 <label className="block mb-2">Email</label>
                 <input
                     type="email"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        emailError && "border-red-500"
+                    }`}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
+                {emailError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-4">
                 <label className="block mb-2">Password</label>
                 <input
                     type="password"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        passwordError && "border-red-500"
+                    }`}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
+                {passwordError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-8">
                 <label className="block mb-2">Konfirmasi Password</label>
                 <input
                     type="password"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        confirmPassError && "border-red-500"
+                    }`}
                     value={confirmPass}
                     onChange={(e) => setConfirmPass(e.target.value)}
                     required
                 />
+                {confirmPassError && (
+                    <p className="text-red-500 text-sm">{confirmPassError}</p>
+                )}
             </div>
             <button className="bg-blue-500 text-white py-2 px-4 rounded-lg w-full hover:bg-blue-600">
                 Register
@@ -146,40 +206,74 @@ function StudentRegisterForm() {
     );
 }
 
+
 function TeacherRegisterForm() {
+    const [birthDate, setBirthDate] = useState("");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [birthDate, setBirthDate] = useState("");
     const [confirmPass, setConfirmPass] = useState("");
+
+    // Error Notification
+    const [error, setError] = useState("");
+    const [nameError, setNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPassError, setConfirmPassError] = useState("");
+
+    const [loading, setLoading] = useState(false);
 
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        setLoading(true);
+
+        // set all error to empty
+        setError("");
+        setNameError("");
+        setEmailError("");
+        setPasswordError("");
+        setConfirmPassError("");
+
+        if (password !== confirmPass) {
+            setConfirmPassError("Password tidak sama");
+            return;
+        }
+
         try {
-            const response = await axios.post(
-                "http://localhost:8000/api/users",
-                {
+            const response = await axios
+                .post("http://localhost:8000/api/register", {
                     name: name,
                     email: email,
                     password: password,
-                    birthdate: birthDate,
+                    birthDate: birthDate,
                     role: "teacher",
-                }
-            );
-            console.log(response.status);
-
-            if (response.status === 201) {
-                // set the token in local storage
-                localStorage.setItem("id_user", response.data.data.id);
-                router.push("/profile");
-            } else {
-                // setError(response.data.message);
-            }
+                })
+                .then((res) => {
+                    if (res.status === 201) {
+                        addToken(res.data.access_token);
+                        // console.log(res.data.access_token);
+                        router.push("/profile");
+                    } else {
+                        setError(res.data.message);
+                    }
+                });
         } catch (error) {
-            // setError(error.response.data.message);
+            console.log(error);
+            if (error.response.data.errors.name) {
+                setNameError(error.response.data.errors.name);
+            } else if (error.response.data.errors.email) {
+                setEmailError(error.response.data.errors.email);
+            } else if (error.response.data.errors.password[0]) {
+                setPasswordError(error.response.data.errors.password[0]);
+            } else {
+                setError(error.response.data.message);
+            }
+
+            setLoading(false);
+
         }
     };
 
@@ -189,16 +283,23 @@ function TeacherRegisterForm() {
             autoComplete="off"
             onSubmit={handleSubmit}
         >
-            <h1 className="text-2xl font-semibold mb-4">Teacher Register</h1>
+            {loading && <LoadingModal showModal={loading} />}
+            <h1 className="text-2xl font-semibold mb-4">Student Register</h1>
+            {error && <p className="text-red-500">{error}</p>}
             <div className="mb-4">
                 <label className="block mb-2">Nama</label>
                 <input
                     type="text"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        nameError && "border-red-500"
+                    }`}
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     required
                 />
+                {nameError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-4">
                 <label className="block mb-2">Tanggal Lahir</label>
@@ -214,31 +315,46 @@ function TeacherRegisterForm() {
                 <label className="block mb-2">Email</label>
                 <input
                     type="email"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        emailError && "border-red-500"
+                    }`}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
+                {emailError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-4">
                 <label className="block mb-2">Password</label>
                 <input
                     type="password"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        passwordError && "border-red-500"
+                    }`}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
+                {passwordError && (
+                    <p className="text-red-500 text-sm">{passwordError}</p>
+                )}
             </div>
             <div className="mb-8">
                 <label className="block mb-2">Konfirmasi Password</label>
                 <input
                     type="password"
-                    className="w-full px-4 py-2 border rounded-lg"
+                    className={`w-full px-4 py-2 border rounded-lg ${
+                        confirmPassError && "border-red-500"
+                    }`}
                     value={confirmPass}
                     onChange={(e) => setConfirmPass(e.target.value)}
                     required
                 />
+                {confirmPassError && (
+                    <p className="text-red-500 text-sm">{confirmPassError}</p>
+                )}
             </div>
             <button className="bg-blue-500 text-white py-2 px-4 rounded-lg w-full hover:bg-blue-600">
                 Register
