d9b53dedaa187038ffddbf5f0343ffa40b160464
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2024-03-28 17:35:46 +0700

    Model Session

---
+++ app/Models/Session.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    // create id that randomly generated
+    
+
+    protected $fillable = [
+        'user_id',
+        'ip_address',
+        'user_agent',
+        'payload',
+        'last_activity',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
