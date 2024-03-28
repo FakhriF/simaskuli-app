cd5d81c64d5df474feef1858598e98533bfd5bfd
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2024-03-28 18:34:13 +0700

    Update form.jsx

--- frontend/app/(auth)/login/form.jsx
+++ frontend/app/(auth)/login/form.jsx
@@ -4,18 +4,21 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { addToken, addUserData } from "@/app/(general)/actions";
+import { LoadingModal } from "@/app/(general)/components/loading";
 
 export default function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [remember, setRemember] = useState(false);
+    // const [remember, setRemember] = useState(false);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         // router.push("/profile");
+        setLoading(true);
 
         try {
             const response = await axios.post(
@@ -23,7 +26,7 @@ export default function LoginForm() {
                 {
                     email: email,
                     password: password,
-                    remember: remember,
+                    // remember: remember,
                 }
             );
             if (response.status === 200) {
@@ -35,6 +38,8 @@ export default function LoginForm() {
             }
         } catch (error) {
             setError(error.response.data.message);
+
+            setLoading(false);
         }
     };
 
@@ -43,6 +48,7 @@ export default function LoginForm() {
             className="bg-white shadow-md rounded-lg p-4 sm:p-12 sm:w-[600px]"
             onSubmit={handleSubmit}
         >
+            {loading && <LoadingModal showModal={loading} />}
             <h1 className="text-2xl font-semibold mb-4">Login</h1>
             {error && <p className="text-red-500">{error}</p>}
             <div className="mb-4">
