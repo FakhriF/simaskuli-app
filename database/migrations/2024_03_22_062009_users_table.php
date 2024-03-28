3af9d1026b1479c3de20893cfed1c833403884d5
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2024-03-28 17:36:13 +0700

    Konsistensi penulisan atribut

--- database/migrations/2024_03_22_062009_users_table.php
+++ database/migrations/2024_03_22_062009_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
-            $table->date('birthdate');
+            $table->date('birthDate');
             $table->rememberToken();
             $table->timestamps();
         });
