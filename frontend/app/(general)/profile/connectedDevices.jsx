16324721d3f1c38a3b282b8545d5142dff3c3fec
Fakhri F <44204754+FakhriF@users.noreply.github.com>
2024-04-01 13:52:33 +0700

    Change loading appearance

--- frontend/app/(general)/profile/connectedDevices.jsx
+++ frontend/app/(general)/profile/connectedDevices.jsx
@@ -72,7 +72,10 @@ export function ProfileConnectedDevicesPage() {
                     </div>
                 ))}
             {sessionData.length === 0 && (
-                <p className="text-center">Loading...</p>
+                // loading blue circle
+                <div className="flex justify-center items-center w-full mt-4">
+                    <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500 border-b-1 border-blue-500"></div>
+                </div>
             )}
         </div>
     );
