091ee49fee52711cf21456bf637f7f2d8e6d7f4c
unknown <radenananta.ram@gmail.com>
2024-04-02 09:41:03 +0700

    Create page.jsx

---
+++ frontend/app/(general)/quiz/page.jsx
@@ -0,0 +1,118 @@
+export default function QuizPage() {
+    return (
+        <div class="body-container">
+            <h1>PELAJARAN LOREM IPSUM</h1>
+            <div class="card" style="margin:10px;">
+                <div class="card-body">
+                    <p style="font-weight:bold;">Question 1</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum, erat a scelerisque ultrices, tortor mauris hendrerit lacus, ac luctus urna quam non quam. Duis eget congue dolor. Praesent sit amet porttitor purus. Suspendisse porttitor efficitur ultricies. Integer quis pulvinar lectus. Nulla pellentesque, metus sed convallis fermentum, justo sapien hendrerit est, eget venenatis justo urna non libero. Curabitur ultrices lorem eros. Quisque aliquam ultricies urna ut accumsan.</p>
+                    <p style="color:gray">Select one:</p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Soal1" id="pilihanASoal1"></input>
+                        <label class="form-check-label" for="pilihanASoal">
+                            Lorem
+                        </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal1" id="pilihanBSoal1"></input>
+                      <label class="form-check-label" for="pilihanBSoal1">
+                            ipsum
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal1" id="pilihanCSoal1"></input>
+                      <label class="form-check-label" for="pilihanCSoal1">
+                            dolor
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal1" id="pilihanDSoal1"></input>
+                      <label class="form-check-label" for="pilihanDSoal1">
+                            sit
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal1" id="pilihanESoal1"></input>
+                      <label class="form-check-label" for="pilihanESoal1">
+                            amet
+                      </label>
+                    </div>
+                </div>
+            </div>
+            <div class="card" style="margin:10px;">
+                <div class="card-body">
+                    <p style="font-weight:bold;">Question 2</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum, erat a scelerisque ultrices, tortor mauris hendrerit lacus, ac luctus urna quam non quam. Duis eget congue dolor. Praesent sit amet porttitor purus. Suspendisse porttitor efficitur ultricies. Integer quis pulvinar lectus. Nulla pellentesque, metus sed convallis fermentum, justo sapien hendrerit est, eget venenatis justo urna non libero. Curabitur ultrices lorem eros. Quisque aliquam ultricies urna ut accumsan.</p>
+                    <p style="color:gray">Select one:</p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Soal2" id="pilihanASoal2"></input>
+                        <label class="form-check-label" for="pilihanASoal2">
+                            Lorem
+                        </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal2" id="pilihanBSoal2"></input>
+                      <label class="form-check-label" for="pilihanBSoal2">
+                            ipsum
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal2" id="pilihanCSoal2"></input>
+                      <label class="form-check-label" for="pilihanCSoal2">
+                            dolor
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal2" id="pilihanDSoal2"></input>
+                      <label class="form-check-label" for="pilihanDSoal2">
+                            sit
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal2" id="pilihanESoal2"></input>
+                      <label class="form-check-label" for="pilihanESoal2">
+                            amet
+                      </label>
+                    </div>
+                </div>
+            </div>
+            <div class="card" style="margin:10px;">
+                <div class="card-body">
+                    <p style="font-weight:bold;">Question 3</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum, erat a scelerisque ultrices, tortor mauris hendrerit lacus, ac luctus urna quam non quam. Duis eget congue dolor. Praesent sit amet porttitor purus. Suspendisse porttitor efficitur ultricies. Integer quis pulvinar lectus. Nulla pellentesque, metus sed convallis fermentum, justo sapien hendrerit est, eget venenatis justo urna non libero. Curabitur ultrices lorem eros. Quisque aliquam ultricies urna ut accumsan.</p>
+                    <p style="color:gray">Select one:</p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Soal3" id="pilihanASoal3"></input>
+                        <label class="form-check-label" for="pilihanASoal3">
+                            Lorem
+                        </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal3" id="pilihanBSoal3"></input>
+                      <label class="form-check-label" for="pilihanBSoal3">
+                            ipsum
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal3" id="pilihanCSoal3"></input>
+                      <label class="form-check-label" for="pilihanCSoal3">
+                            dolor
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal3" id="pilihanDSoal3"></input>
+                      <label class="form-check-label" for="pilihanDSoal3">
+                            sit
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="Soal3" id="pilihanESoal3"></input>
+                      <label class="form-check-label" for="pilihanESoal3">
+                            amet
+                      </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
