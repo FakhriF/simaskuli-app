472a9c903e7ecfb075497dda00017dee2e44e767
unknown <radenananta.ram@gmail.com>
2024-04-20 14:35:24 +0700

    Update page.jsx

--- frontend/app/(general)/quiz/page.jsx
+++ frontend/app/(general)/quiz/page.jsx
@@ -1,31 +1,58 @@
 export default function QuizPage() {
-    return (
-        <div className="w-full p-4 bg-gray-100 min-h-screen">
-            <h1 className="text-3xl font-bold my-8">PELAJARAN LOREM IPSUM</h1>
-            <div>
-                <div className="border border-gray-300 rounded-lg mb-4">
-                    <div className="text-white bg-gray-800 p-4 rounded-t-lg">
-                        <h1 className="text-xl font-bold">Question 1</h1>
-                    </div>
-                <div className="p-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum, erat a scelerisque ultrices, tortor mauris hendrerit lacus, ac luctus urna quam non quam. Duis eget congue dolor. Praesent sit amet porttitor purus. Suspendisse porttitor efficitur ultricies. Integer quis pulvinar lectus. Nulla pellentesque, metus sed convallis fermentum, justo sapien hendrerit est, eget venenatis justo urna non libero. Curabitur ultrices lorem eros. Quisque aliquam ultricies urna ut accumsan.</p>
-                    <p>Select one:</p>
-                    <div className="my-4">
-        <h2 className="text-xl font-bold mb-2">Modules</h2>
-        {[...Array(4).keys()].map((module) => (
-          <div key={module} className="border border-gray-300 rounded-lg p-4 mb-4 hover:bg-cyan-200 hover:border-blue-500">
-            <h3 className="text-lg font-bold mb-2">Module {module + 1}</h3>
-            <ul className="list-disc pl-6 mt-2">
-              <li>Lecture</li>
-              <li>Assignment</li>
-              <li>Discussion</li>
-            </ul>
+  return (
+      <div className="mx-auto">
+          <div className="bg-transparent border-b border-gray-300">
+              <h1 className="text-xl font-bold mx-4 my-4">Nama mata pelajaran</h1>
           </div>
-        ))}
+          <div className="flex flex-col items-left justify-left max-w-7xl mx-auto my-24 p-6 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+              <div className="w-full mx-auto mb-4">
+                  <img src="https://cdn0-production-images-kly.akamaized.net/9BT-b8pZky5Vvmr282XgkIG3Rgc=/1200x1200/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/1426560/original/015842000_1480926209-namaafrikacov.jpg" className="w-40 h-50 object-cover rounded-lg" alt="quiz-image" />
+              </div>
+              <p className="text-xl font-bold">Ceritanya soal bergambar?</p>
+              <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi waktu dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+              <div className="border-t border-gray-300 py-4">
+                  <button className="float-left hover:bg-blue-500 hover:text-white bg-gray-300 text-gray-400 font-bold py-2 px-4 rounded">
+                      Kembali
+                  </button>
+                  <button className="float-right hover:bg-blue-500 hover:text-white bg-blue-500 text-white font-bold py-2 px-4 rounded">
+                      Lanjutkan
+                  </button>
+              </div>
+          </div>
+          {/*<div className="flex flex-col items-left justify-left max-w-7xl mx-auto my-24 p-6 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+              <p className="text-xl font-bold">Ceritanya soal?</p>
+              <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi waktu dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+              <div className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded my-4">
+                  <button>Opsi gak dipilih</button>
+              </div>
+          </div>
+          <div className="px-4 py-3">
+              <button className="float-left hover:bg-blue-500 hover:text-white bg-gray-300 text-gray-400 font-bold py-2 px-4 rounded">
+                  Previous
+              </button>
+              <button className="float-right hover:bg-blue-500 hover:text-white bg-gray-300 text-gray-400 font-bold py-2 px-4 rounded">
+                  Next
+              </button>
+  </div>*/}
       </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    );
+  );
 }
